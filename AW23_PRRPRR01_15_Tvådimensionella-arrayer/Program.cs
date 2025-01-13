0f547b2ae97c5872360ba2b03dec20d46c112ba1
magisterblaman <hannes.blaman@lbs.se>
2025-01-13 14:47:13 +0100

    Grannar!

--- AW23_PRRPRR01_15_Tvådimensionella-arrayer/Program.cs
+++ AW23_PRRPRR01_15_Tvådimensionella-arrayer/Program.cs
@@ -6,6 +6,35 @@ using System.Threading.Tasks;
 
 namespace AW23_PRRPRR01_15_Tvådimensionella_arrayer {
 	internal class Program {
+		static void FillCellWithSumOfSurroundingCells(int[,] array, int x, int y) {
+			int sum = 0;
+			//sum += array[x + (-1), y + (-1)];
+			//sum += array[x + 0, y + (-1)];
+			//sum += array[x + 1, y + (-1)];
+			//sum += array[x + 1, y + 0];
+			//sum += array[x + 1, y + 1];
+			//sum += array[x + 0, y + 1];
+			//sum += array[x + (-1), y + 1];
+			//sum += array[x + (-1), y + 0];
+
+			for (int yOffset = -1; yOffset <= 1; yOffset++) {
+				for (int xOffset = -1; xOffset <= 1; xOffset++) {
+					if (yOffset == 0 && xOffset == 0) {
+						continue; // hoppar över den här iterationen (körningen) av den inre for-loopen
+					}
+					if (x + xOffset < 0 || x + xOffset >= array.GetLength(0)
+						|| y + yOffset < 0 || y + yOffset >= array.GetLength(1)) {
+
+						continue;
+					}
+
+					sum += array[x + xOffset, y + yOffset];
+				}
+			}
+
+			array[x, y] = sum;
+		}
+
 		static void Main(string[] args) {
 			//int[,] myArray = new int[6, 5] {
 			//	{ 7, 6, 5, 4, 3 },
@@ -40,6 +69,15 @@ namespace AW23_PRRPRR01_15_Tvådimensionella_arrayer {
 				{ 20, 1, 58, 69, 16 },
 				{ 11, 94, 55, 18, 12 }
 			};
+
+			FillCellWithSumOfSurroundingCells(array, 2, 4);
+
+			for (int y = 0; y < array.GetLength(1); y++) {
+				for (int x = 0; x < array.GetLength(0); x++) {
+					Console.Write(array[x, y] + " ");
+				}
+				Console.WriteLine();
+			}
 
 		}
 	}
