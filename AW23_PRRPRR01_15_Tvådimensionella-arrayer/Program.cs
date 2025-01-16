759caaa4ebe49fb43baf4094efd90a6d763b2a4e
magisterblaman <hannes.blaman@lbs.se>
2025-01-16 12:21:17 +0100

    Bild!

--- AW23_PRRPRR01_15_Tvådimensionella-arrayer/Program.cs
+++ AW23_PRRPRR01_15_Tvådimensionella-arrayer/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Pastel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace AW23_PRRPRR01_15_Tvådimensionella_arrayer {
 	internal class Program {
@@ -35,50 +37,33 @@ namespace AW23_PRRPRR01_15_Tvådimensionella_arrayer {
 			array[x, y] = sum;
 		}
 
+		static void MakeGrayscale(Color[,] image) {
+			for (int y = 0; y < image.GetLength(1); y++) {
+				for (int x = 0; x < image.GetLength(0); x++) {
+					Color oldColor = image[x, y];
+
+					int average = (oldColor.R + oldColor.G + oldColor.B) / 3;
+
+					Color newColor = Color.FromArgb(oldColor.A, average, average, average);
+
+					image[x, y] = newColor;
+				}
+			}
+		}
+
 		static void Main(string[] args) {
-			//int[,] myArray = new int[6, 5] {
-			//	{ 7, 6, 5, 4, 3 },
-			//	{ -3, -4, -5, -6, -7 },
-			//	{ 15, 14, 13, 12, 11 },
-			//	{ 28, 27, 26, 25, 24 },
-			//	{ 99, 98, 97, 96, 95 },
-			//	{ 0, -1, -2, -3, -4 }
-			//};
-			//myArray[0, 0] = 7;
-			//myArray[1, 0] = -3;
-			//myArray[2, 0] = 15;
+			//Console.WriteLine("Hejsan svejsan".PastelBg(Color.FromArgb(23, 98, 213)));
 
-			////for (int i = 0; i < myArray.Length; i++) {
-			////	Console.WriteLine(myArray[i]);
-			////}
+			Color[,] image = ImageHandler.LoadImage("diamond_pickaxe.png");
 
-			//for (int y = 0; y < myArray.GetLength(1); y++) {
-			//	for (int x = 0; x < myArray.GetLength(0); x++) {
-			//		Console.Write(myArray[x, y] + " ");
-			//	}
-			//	Console.WriteLine();
-			//}
+			MakeGrayscale(image);
 
-			////myArray[2, 3]
-
-			int[,] array = new int[,] {
-				{ 17, 67, 83, 9, 1337},
-				{ 35, 42, 39, 97, 31 },
-				{ 87, 27, 61, 76, 420 },
-				{ -15, 2, 72, 84, 5 },
-				{ 20, 1, 58, 69, 16 },
-				{ 11, 94, 55, 18, 12 }
-			};
-
-			FillCellWithSumOfSurroundingCells(array, 2, 4);
-
-			for (int y = 0; y < array.GetLength(1); y++) {
-				for (int x = 0; x < array.GetLength(0); x++) {
-					Console.Write(array[x, y] + " ");
+			for (int y = 0; y < image.GetLength(1); y++) {
+				for (int x = 0; x < image.GetLength(0); x++) {
+					Console.Write(" ".PastelBg(image[x, y]));
 				}
 				Console.WriteLine();
 			}
-
 		}
 	}
 }
